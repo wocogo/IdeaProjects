0ebb9d6d5138ecab30d921385ec5173405dfd0c1
WolfgangW <wolfgangw@synnex.com>
2017-09-19 17:13:52 +0800

    spring boot hibernate demo

--- springboot-hibernate/src/main/java/com/example/demo/web/UserController.java
+++ springboot-hibernate/src/main/java/com/example/demo/web/UserController.java
@@ -1,5 +1,8 @@
 package com.example.demo.web;
 
+import com.example.demo.dao.UserDao;
+import com.example.demo.entity.UserEntity;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,10 +13,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/home")
 public class UserController {
+    @Autowired
+    UserDao userDao;
+
     @RequestMapping(value = "/user2")
     @ResponseBody
-    public String sayHi(){
-        return "Hi";
+    public String sayHi(String userName){
+        String userId;
+        UserEntity userEntity = userDao.findByName(userName);
+        if(userEntity != null){
+            return userName + " : nickName: " + userEntity.getNickName();
+        }else{
+            return userName + " does not exists";
+        }
     }
 
 }
