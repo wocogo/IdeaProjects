7bd18116423f7f408187b0b76f0e5d50b8b2712f
wocogo <wocogo@163.com>
2017-10-20 00:00:22 +0800

    springboot-shiro demo

--- demo-shiro/src/main/java/com/example/demo/controller/AdminController.java
+++ demo-shiro/src/main/java/com/example/demo/controller/AdminController.java
@@ -1,12 +1,17 @@
 package com.example.demo.controller;
 
+import com.example.demo.dao.UUserDao;
 import com.example.demo.entity.UUserEntity;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -17,6 +22,9 @@ import java.util.Map;
  */
 @Controller
 public class AdminController {
+    @Autowired
+    UUserDao uUserDao;
+
     @RequestMapping(value = "admin/mgmt")
     public String login(){
         return "admin/mgmt";
@@ -34,12 +42,21 @@ public class AdminController {
 
     @RequestMapping(value = "admin/user/edit", method = RequestMethod.POST)
     @ResponseBody
-    public Map<String,String> editUser(HttpServletRequest request){
+    public Map<String,String> editUser(HttpServletRequest request) throws IOException {
         String userEntryListJson = request.getParameter("data");
+
 //        userEntityList = JacksonMapper
-        ArrayList<UUserEntity> userEntityList = null;
+//        Jackson2ObjectMapperBuilder.json().json().json().build().readvalue
+        ArrayList<UUserEntity> userEntityList1 = null;
+        ObjectMapper mapper = new ObjectMapper();
+        List<UUserEntity> userEntityList = mapper.readValue(userEntryListJson, new TypeReference<List<UUserEntity>>(){});
+        System.out.println(userEntryListJson);
         System.out.println(userEntityList);
+        System.out.println("userEntityList.size()= "+ userEntityList.size());
+        System.out.println(userEntityList.get(0).getEmail());
+        uUserDao.save(userEntityList);
         Map<String,String> returnMap = new HashMap<>();
+
         return returnMap;
     }
 
--- demo-shiro/src/main/java/com/example/demo/dao/UUserDao.java
+++ demo-shiro/src/main/java/com/example/demo/dao/UUserDao.java
@@ -4,10 +4,15 @@ import com.example.demo.entity.UUserEntity;
 import org.springframework.data.repository.CrudRepository;
 
 import javax.transaction.Transactional;
+import java.util.List;
 
 /**
  * Created by Administrator on 2017/9/27.
  */
 @Transactional
 public interface UUserDao extends CrudRepository<UUserEntity, Long>{
+
+    public List<UUserEntity> save(List<UUserEntity> uUserEntities);
+
+    public UUserEntity save(UUserEntity uUserEntity);
 }
