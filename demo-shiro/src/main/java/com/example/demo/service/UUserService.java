976a4c6ddf44d68899bfd3d0ac80efd3a5127fe6
WolfgangW <wolfgangw@synnex.com>
2018-02-22 08:33:13 +0800

    fix dynamic showPage in spring boot

--- demo-shiro/src/main/java/com/example/demo/service/UUserService.java
+++ demo-shiro/src/main/java/com/example/demo/service/UUserService.java
@@ -1,9 +1,10 @@
 package com.example.demo.service;
 
-import com.example.demo.entity.UUserEntity;
+import com.qexample.demo.entity.UUserEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+
 
 /**
  * Created by Administrator on 2017/10/23.
