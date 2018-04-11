bf15aa851b7f3ab3778c8774d5dbf1a5d4ae267b
WolfgangW <wolfgangw@synnex.com>
2018-04-11 14:55:52 +0800

    Fast running for test

---
+++ FastRunning/src/main/java/cn/helssm/test/IAddAdd.java
@@ -0,0 +1,40 @@
+package cn.helssm.test;
+
+/**
+ * Created by WolfgangW on 04/11/18.
+ */
+public class IAddAdd {
+
+    public static void main(String[] args) {
+//        int i = 0;
+//        i++;
+//        System.out.println(i);
+//        System.out.println(i);
+//        int j = 5;
+//        ++j;
+//        System.out.println(j);
+//        System.out.println(j);
+//        ================
+
+//        System.out.println(5.00 - 4.90);
+//        System.out.println(300 - 210);
+//
+//        ==================
+//        OverLoad o = new OverLoad();
+//        o.test(null);
+//
+//        =================
+
+    }
+
+
+}
+class OverLoad{
+    public void test(Object o) {
+        System.out.println("new Object");
+    }
+
+    public void test(String s){
+        System.out.println("new String");
+    }
+}
