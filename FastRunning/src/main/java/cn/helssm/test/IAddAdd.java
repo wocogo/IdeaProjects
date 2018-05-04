aea646eb1cfaf85f5abde0129363c76086de9585
wocogo <wocogo@163.com>
2018-05-04 22:04:31 +0800

    java tests case

--- FastRunning/src/main/java/cn/helssm/test/IAddAdd.java
+++ FastRunning/src/main/java/cn/helssm/test/IAddAdd.java
@@ -24,7 +24,25 @@ public class IAddAdd {
 //        o.test(null);
 //
 //        =================
+//        System.out.println("|DF|A3".split("|").length);
 
+        System.out.println("Length: " +  "|DF|A3".split("|").length);
+
+
+        String [] xx =  "|DF|A3".split("|");
+        for(int i = 0; i < xx.length; i++)
+        System.out.println(xx[i]);
+        System.out.println("===");
+        String str="abcdefg";
+        System.out.println("indexof: "+ str.substring(2,5).indexOf('d'));
+
+//        Char c3 = 'x';
+//        char c4 = '';
+//        char c1='\"';
+//        char c5=65;
+//        System.out.println(c3);
+//        System.out.println(c1);
+//        System.out.println(c5);
     }
 
 
@@ -37,4 +55,25 @@ class OverLoad{
     public void test(String s){
         System.out.println("new String");
     }
+
+//    public void modify() {
+//        int I, j, k;
+//        I = 100;
+//        while (I > 0) {
+//            j = I * 2;
+//            System.out.println("Thevalueofjis" + j);
+//            k = k + 1;
+//            I--;
+//        }
+//    }
+
 }
+
+
+//class Test {
+//    public static void main(String[] args) {
+//        byte b = 2, e = 3; //------1
+//        byte f = b + e;//------2
+//        System.out.println(f);
+//    }
+//}
